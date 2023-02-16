4a0e8a35ce6a1ae75d853a7618fc8c4c8d8d90d3
NesterovEvgeniy <105553884+NesterovEvgeniy@users.noreply.github.com>
2023-02-16 11:33:28 +0500

    Clothes Shop(The brend)
    Add new project

---
+++ Clothes Shop(The brend)/JSON/data.js
@@ -0,0 +1,64 @@
+const dataProducts = `
+    [
+      { 
+        "id": 1,   
+        "title": "AUTUMN JACKET",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "115.90",
+        "color": "Blue", 
+        "size": "L",
+        "quantity": "1",
+        "image": "img/product-1.svg"
+      },
+      { 
+        "id": 2,      
+        "title": "OFFICE PANTSUIT",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "269.90",
+        "color": "Black", 
+        "size": "M",
+        "quantity": "1",
+        "image": "img/product-2.svg"          
+      }, 
+      { 
+        "id": 3,      
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "49.90",
+        "color": "Black/Blue", 
+        "size": "XL",
+        "quantity": "1",
+        "image": "img/product-3.svg"           
+      },
+      { 
+        "id": 4,      
+        "title": "MODERN PANTS",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "64.90",
+        "color": "Sand", 
+        "size": "L",
+        "quantity": "1",
+        "image": "img/product-4.svg"            
+      },
+      { 
+        "id": 5,     
+        "title": "MODERN JACKET",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "79.90",
+        "color": "Light Blue", 
+        "size": "M",
+        "quantity": "1",
+        "image": "img/product-5.svg"            
+      },
+      { 
+        "id": 6,       
+        "title": "SHORT SLEEVE SHIRT",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "39.90",
+        "color": "Light Green", 
+        "size": "S",
+        "quantity": "1",
+        "image": "img/product-6.svg"            
+      }
+    ]
+`
