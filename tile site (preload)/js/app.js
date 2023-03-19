002c8553da727ee62eeebdfb5423f07b2b276e40
NesterovEvgeniy <105553884+NesterovEvgeniy@users.noreply.github.com>
2023-03-19 22:58:24 +0500

    Add tile-site

---
+++ tile site (preload)/js/app.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+
+  setTimeout(function () {
+
+    document.body.classList.add('loaded')
+
+    Draggable.create('.gallery', {
+      bounds: 'body',
+      inertia: true
+    });
+
+
+
+  })
+
+
+}
